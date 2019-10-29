e77f816f980d4ded53dd39ca7827ab647d97d75f
Default user <id.vargas@uniandes.edu.co>
2019-10-29 21:53:37 +0000

    inicial

---
+++ archivo2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    a = 3;
+    b = 4;
+    c = 5;
+    if(pow(a,2) + pow(b,2) == pow(c,2)){
+        cout << "Si";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+    
+}
