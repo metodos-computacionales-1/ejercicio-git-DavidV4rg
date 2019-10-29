302c8125e69e359e2ef253ff6e62fc9b596f0965
Default user <id.vargas@uniandes.edu.co>
2019-10-29 21:57:21 +0000

    terminado

--- archivo2.cpp
+++ archivo2.cpp
@@ -6,14 +6,27 @@ int main(){
     int a;
     int b;
     int c;
+    int c1;
     a = 3;
     b = 4;
     c = 5;
     if(pow(a,2) + pow(b,2) == pow(c,2)){
         cout << "Si";
+        cout <<"\n";
     }
     else {
         cout << "no";
+    }
+    cout << "Con otros valores:";
+    cout <<"\n";
+        
+    c1 = 6;
+    if(pow(a,2) + pow(b,2) == pow(c1,2)){
+        cout << "Si";
+        cout <<"\n";
+    }
+    else {
+        cout << "No";
     }
     return 0;
     
