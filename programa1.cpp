1a452a603833018a1bd08f7c056d2420af379a47
Default user <id.vargas@uniandes.edu.co>
2019-10-29 21:40:11 +0000

    rueba inicial

---
+++ programa1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double n;
+    double Pi = 3.14159265359;
+    cout << "Ingrese el valr del radio del círculo: ";
+    cin >> n;
+    
+    cout << "El perimetro es: " << 2*Pi*n<<endl;
+}
